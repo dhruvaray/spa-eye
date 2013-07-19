83b52cf33540545980f526bd13da5654ff2c1dce
dhruvaray <dhruva_ray@persistent.co.in>
2013-07-19 18:24:49 +0530

    Show data and paritals in script view

--- extension/chrome/content/spa_eye/panels/viewPanel.js
+++ extension/chrome/content/spa_eye/panels/viewPanel.js
@@ -5,87 +5,88 @@ define([
     "firebug/lib/locale",
     "firebug/lib/domplate",
 
-    "spa_eye/panels/basePanel",
+
     "spa_eye/dom/section",
-    "spa_eye/dom/modelReps"
+    "spa_eye/dom/modelReps",
+
+    "spa_eye/panels/basePanel"
 ],
-function (Firebug, Obj, FBTrace, Locale, Domplate) {
+    function (Firebug, Obj, FBTrace, Locale, Domplate, ChildSection, ModelReps) {
 
-    var viewPanel = Firebug.viewPanel = function () {
-    };
+        var viewPanel = Firebug.viewPanel = function () {
+        };
 
-    viewPanel.prototype = Obj.extend(Firebug.Panel, {
-        name:"view",
-        title:Locale.$STR("spa_eye.script.view.title"),
+        viewPanel.prototype = Obj.extend(Firebug.Panel, {
+            name:"view",
+            title:Locale.$STR("spa_eye.script.view.title"),
 
-        parentPanel:"script",
-        order:4,
+            parentPanel:"script",
+            order:4,
 
-        initialize:function () {
-            Firebug.Panel.initialize.apply(this, arguments);
-        },
-
-        destroy:function (state) {
-            Firebug.Panel.destroy.apply(this, arguments);
-        },
-
-        updateSelection:function (frame) {
-            // this method is called while the debugger has halted JS,
-            // so failures don't show up in FBS_ERRORS
-            try {
-                this.show(frame);
-            }
-            catch (exc) {
-                if (FBTrace.DBG_ERRORS && FBTrace.DBG_STACK)
-                    FBTrace.sysout("updateSelection FAILS " + exc, exc);
-            }
-        },
-
-        show:function () {
-            var source = ["obj", "__p"];
-            var output =[
-                Locale.$STR("spa_eye.script.view.nodata"),
-                Locale.$STR("spa_eye.script.view.noviewselected")
-            ];
-            var context = this.context;
-
-            for(var i=0;i<source.length;++i)
-                Firebug.CommandLine.evaluate(source[i], context, null, context.getCurrentGlobal(),
-                    function success(result, context) {
-                        output[i] = result;
-                    },
-                    function failed(result, context) {
-                        if (result.source !== source || result.name !== "ReferenceError")
-                            output[i] = exc.message;
-                    }
-                );
-
-
-            this.panelNode = partialHTML;
-
-        }
-
-    });
-
-    with (Domplate) {
-
-        Firebug.viewPanel.prototype.view_template = domplate({
-            tag:DIV({onclick:"$handleClick", class:"$data|computeVisibility"}, Locale.$STR("spa_eye.reload")),
-
-            handleClick:function (event) {
-
-                Firebug.Options.setPref("javascript", "enabled", true);
-                Firebug.TabWatcher.reloadPageFromMemory(Firebug.currentContext);
+            initialize:function () {
+                Firebug.Panel.initialize.apply(this, arguments);
             },
 
-            computeVisibility:function (data) {
-                return "show";//for now
+            destroy:function (state) {
+                Firebug.Panel.destroy.apply(this, arguments);
+            },
+
+            updateSelection:function (frame) {
+                // this method is called while the debugger has halted JS,
+                // so failures don't show up in FBS_ERRORS
+                try {
+                    this.show(frame);
+                }
+                catch (exc) {
+                    if (FBTrace.DBG_ERRORS && FBTrace.DBG_STACK)
+                        FBTrace.sysout("updateSelection FAILS " + exc, exc);
+                }
+            },
+
+            show:function () {
+                var source = ["obj", "__p"];
+                var output = [
+                    Locale.$STR("spa_eye.script.view.nodata"),
+                    Locale.$STR("spa_eye.script.view.noviewselected")
+                ];
+                var context = this.context;
+
+                for (var i = 0; i < source.length; ++i)
+                    Firebug.CommandLine.evaluate(source[i], context, null, context.getCurrentGlobal(),
+                        function success(result, context) {
+                            output[i] = result;
+                        },
+                        function failed(result, context) {
+                            if (result.source !== source || result.name !== "ReferenceError")
+                                output[i] = exc.message;
+                        }
+                    );
+
+
+                var args = {
+                    sections:[
+                        new ChildSection({
+                            name:'data',
+                            title:Locale.$STR("spa_eye.script.view.data"),
+                            parent:this.panelNode,
+                            data:output[0]
+                        }),
+                        new ChildSection({
+                            name:'view',
+                            title:Locale.$STR("spa_eye.script.view.template"),
+                            parent:this.panelNode,
+                            data:{HTML:output[1]}
+                        })
+                    ],
+                    mainPanel:this
+                };
+                ModelReps.DirTablePlate.tag.replace(args, this.panelNode);
+
             }
 
         });
-    }
 
-    Firebug.registerPanel(Firebug.viewPanel);
-    return Firebug.viewPanel;
+        Firebug.registerPanel(Firebug.viewPanel);
+        return Firebug.viewPanel;
 
-});
+    });
