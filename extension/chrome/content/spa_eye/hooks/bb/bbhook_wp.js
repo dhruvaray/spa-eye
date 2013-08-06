402afb02fc8dbfe320cdc196084fc94435fe9886
Jaynti Kanani <jdkanani@gmail.com>
2013-08-06 15:20:38 +0530

    add safety checks for bbhook

--- extension/chrome/content/spa_eye/hooks/bb/bbhook_wp.js
+++ extension/chrome/content/spa_eye/hooks/bb/bbhook_wp.js
@@ -6,9 +6,13 @@ if (window.Backbone) {
     if (_ModelProxy) {
         var _ModelProxyProto = window.Backbone.Model.prototype;
         window.Backbone.Model = function (attributes, options) {
-            window.spa_eye.models = window.spa_eye.models || [];
-            window.spa_eye.models.push(this);
-            _ModelProxy.apply(this, arguments);
+            try {
+                window.spa_eye.models = window.spa_eye.models || [];
+                window.spa_eye.models.push(this);
+            } catch (e) {
+            } finally {
+                _ModelProxy.apply(this, arguments);
+            }
         };
         window.Backbone.Model.prototype = _ModelProxyProto;
         _.extend(window.Backbone.Model, _ModelProxy);
@@ -19,10 +23,14 @@ if (window.Backbone) {
     if (_colProxy) {
         var _colProxyProto = window.Backbone.Collection.prototype;
         window.Backbone.Collection = function (attributes, options) {
-            this.cid = this.cid || window._.uniqueId('c')
-            window.spa_eye.collections = window.spa_eye.collections || [];
-            window.spa_eye.collections.push(this);
-            _colProxy.apply(this, arguments);
+            try {
+                this.cid = this.cid || window._.uniqueId('c')
+                window.spa_eye.collections = window.spa_eye.collections || [];
+                window.spa_eye.collections.push(this);
+            } catch (e) {
+            } finally {
+                _colProxy.apply(this, arguments);
+            }
         };
         window.Backbone.Collection.prototype = _colProxyProto;
         _.extend(window.Backbone.Collection, _colProxy);
@@ -35,26 +43,28 @@ if (window.Backbone) {
         window.Backbone.View = function (attributes, options) {
             var renderProxy = this.render;
             var removeProxy = this.remove;
-            this.render = function () {
-                window.spa_eye.cv = this;
-                this.inferredTemplates = this.inferredTemplates || [];
-                window.spa_eye.path.push(this);
-                var result = renderProxy.apply(this, arguments);
-                window.spa_eye.path.pop();
-                window.spa_eye.cv = undefined;
-                return result;
-            };
+            try{
+                this.render = function () {
+                    window.spa_eye.cv = this;
+                    this.inferredTemplates = this.inferredTemplates || [];
+                    window.spa_eye.path.push(this);
+                    var result = renderProxy.apply(this, arguments);
+                    window.spa_eye.path.pop();
+                    window.spa_eye.cv = undefined;
+                    return result;
+                };
 
-            this.remove = function () {
-                this.mfd = true;
-                return removeProxy.apply(this, arguments);
-            };
+                this.remove = function () {
+                    this.mfd = true;
+                    return removeProxy.apply(this, arguments);
+                };
 
-            window.spa_eye.views = window.spa_eye.views || [];
-            window.spa_eye.views.push(this);
-
-            _viewProxy.apply(this, arguments);
-
+                window.spa_eye.views = window.spa_eye.views || [];
+                window.spa_eye.views.push(this);
+            } catch (e) {
+            } finally {
+                _viewProxy.apply(this, arguments);
+            }
         };
         window.Backbone.View.prototype = _viewProxyProto;
         _.extend(window.Backbone.View, _viewProxy);
