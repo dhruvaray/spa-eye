34db0201184adbfb7aa5716d51cf36c9f4f11a0b
Dhruva Ray <dhruvaray@gmail.com>
2013-08-10 19:04:11 +0530

    Generalize bbhook_wp

--- extension/chrome/content/spa_eye/hooks/bb/bbhook_wp.js
+++ extension/chrome/content/spa_eye/hooks/bb/bbhook_wp.js
@@ -9,58 +9,41 @@ if (window.Backbone) {
         var id = ++this.idCounter + '';
         return prefix ? prefix + id : id;
     };
-    //var proxyable = [window.Backbone.Model, window.Backbone.Collection, window.Backbone.View];
+    window.spa_eye.proxyable = ['Model', 'Collection', 'View'];
+    window.spa_eye.proxy = {};
+    window.spa_eye.proxyproto = {};
+    window.spa_eye.store = ['models', 'collections', 'views'];
+    window.spa_eye.prefix = ['c', 'col', 'view'];
+    for (var i = 0; i < window.spa_eye.store.length; ++i)
+        window.spa_eye[window.spa_eye.store[i]] = [];
 
-    var _ModelProxy = window.Backbone.Model;
-    if (_ModelProxy) {
-        var _ModelProxyProto = window.Backbone.Model.prototype;
-        window.Backbone.Model = function (attributes, options) {
-            window.spa_eye.models = window.spa_eye.models || [];
-            window.spa_eye.models.push(this);
-            var result = _ModelProxy.apply(this, arguments);
-            var event = new CustomEvent('Backbone_Eye:ADD', {'detail':{'type':'Model', data:this}});
-            window.dispatchEvent(event);
-            return result;
-        };
-        window.Backbone.Model.prototype = _ModelProxyProto;
-        _.extend(window.Backbone.Model, _ModelProxy);
+    for (var i = 0; i < window.spa_eye.proxyable.length; ++i) {
+        (function (key, entity, prefix) {
+            window.spa_eye.proxy[entity] = window.Backbone[entity];
+            if (window.spa_eye.proxy[entity]) {
+                window.spa_eye.proxyproto[entity] = window.Backbone[entity].prototype;
+                window.Backbone[entity] = function (attributes, options) {
+                    console.log("Entity : " + entity);
+                    console.log("Key : " + key);
+                    console.log("Backbone Entity : " + window.Backbone[entity]);
+                    console.log("Backbone Proxy Entity : " + window.spa_eye.proxy[entity]);
+                    console.log(this);
+                    console.log(this instanceof  window.Backbone[entity]);
+                    this.cid = this.cid || (typeof(window._) === "undefined")
+                        ? window.spa_eye.uniqueId(prefix)
+                        : window._.uniqueId(prefix);
+
+                    window.spa_eye[key].push(this);
+                    var result = window.spa_eye.proxy[entity].apply(this, arguments);
+                    var event = new CustomEvent('Backbone_Eye:ADD', {'detail':{'type':entity, data:this}});
+                    window.dispatchEvent(event);
+                    return result;
+                };
+                window.Backbone[entity].prototype = window.spa_eye.proxyproto[entity];
+                _.extend(window.Backbone[entity], window.spa_eye.proxy[entity]);
+            }
+            ;
+        })(window.spa_eye.store[i], window.spa_eye.proxyable[i], window.spa_eye.prefix[i]);
     }
-    ;
-
-    var _colProxy = window.Backbone.Collection;
-    if (_colProxy) {
-        var _colProxyProto = window.Backbone.Collection.prototype;
-        window.Backbone.Collection = function (attributes, options) {
-            this.cid = this.cid || (typeof(window._) === "undefined")
-                ? window.spa_eye.uniqueId('col')
-                : window._.uniqueId('col');
-            window.spa_eye.collections = window.spa_eye.collections || [];
-            window.spa_eye.collections.push(this);
-            var result = _colProxy.apply(this, arguments);
-            var event = new CustomEvent('Backbone_Eye:ADD', {'detail':{'type':'Collection', data:this}});
-            window.dispatchEvent(event);
-            return result;
-        };
-        window.Backbone.Collection.prototype = _colProxyProto;
-        _.extend(window.Backbone.Collection, _colProxy);
-    }
-    ;
-
-    var _viewProxy = window.Backbone.View;
-    if (_viewProxy) {
-        var _viewProxyProto = window.Backbone.View.prototype;
-        window.Backbone.View = function (attributes, options) {
-            window.spa_eye.views = window.spa_eye.views || [];
-            window.spa_eye.views.push(this);
-            var result = _viewProxy.apply(this, arguments);
-            var event = new CustomEvent('Backbone_Eye:ADD', {'detail':{'type':'View', data:this}});
-            window.dispatchEvent(event);
-            return result;
-        };
-        window.Backbone.View.prototype = _viewProxyProto;
-        _.extend(window.Backbone.View, _viewProxy);
-
-    }
-    ;
 }
 ;
