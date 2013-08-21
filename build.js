336c50e2c11f97e29970ec5ed144a398611898d7
Jaynti Kanani <jdkanani@gmail.com>
2013-08-21 17:37:30 +0530

    added testcase boilerplate code

--- build.js
+++ build.js
@@ -1,34 +1,57 @@
 // ********************************************************************************************* //
 
-var path = require("path");
-var fs = require("fs");
-var os = require("os");
-var exec = require("child_process").exec;
+var http = require("http"),
+    path = require("path"),
+    fs = require("fs");
+    url = require("url"),
+    os = require("os"),
+    exec = require("child_process").exec;
 
 // ********************************************************************************************* //
 // Globals
 
-var version = null;
+var version = null,
+    port = 8888,
+    testUrl = "http://localhost:@PORT@/test/spa_eye.html";
 
 // Compute various target directories.
-var extensionDir = "./extension";
+var currentDir = ".",
+    extensionDir = "./extension",
+    testDir = "./test";
 
-var buildDir = "./build";
-var nightlyDir = "./nightly";
-var releaseDir = "./releases";
+var buildDir = "./build",
+    nightlyDir = "./nightly",
+    releaseDir = "./releases";
 
 // ********************************************************************************************* //
 
 function help () {
     console.log('Usage:');
     console.log('');
-    console.log('1. In order to build Firebug xpi run:');
+    console.log('1. In order to build `spa_eye` xpi run:');
+    console.log('');
     console.log('       $ node build.js');
+    console.log('');
     console.log('   The final xpi + update.rdf file will be located in the \'releases\' sub directory.');
     console.log('');
-    console.log('2. In order to build Firebug final release run:');
+    console.log('2. In order to build `spa_eye` final release run:');
+    console.log('');
     console.log('       $ node build.js nightly');
+    console.log('');
     console.log('   Again xpi files will be located in the \'nightly\' directory.');
+    console.log('');
+    console.log('3. Run testcase for `spa_eye`:');
+    console.log('');
+    console.log('       $ node build.js test <PROFILE> <PORT>');
+    console.log('');
+    console.log('   <PORT> is the port for local server. Default port is `8888`');
+    console.log('   <PROFILE> ist the firefox profile name to run testcases. Default profile name is `dev`');
+    console.log('');
+    console.log('4. Start simple server to run test cases manually');
+    console.log('');
+    console.log('       $ node build.js server <PORT>');
+    console.log('');
+    console.log('   <PORT> is the port for local server. Default port is `8888`');
     console.log('');
 }
 
@@ -40,12 +63,29 @@ function main () {
 
     if (args.length < 3) {
         build(releaseDir);
-    } else if (args.length >= 4 || args[2] === "help") {
+    } else if (args[2] === "help") {
         help();
     } else if (args[2] === "clean") {
         clean();
     } else if (args[2] === "nightly") {
         build(nightlyDir);
+    } else if (args[2] === "test") {
+        var profile = args[3] || 'dev';
+
+        // Setting global values
+        port = parseInt(args[4] || port, 10);
+        testUrl = testUrl.replace(/@PORT@/g, port);
+
+        // Run test case
+        runTest(profile);
+
+    } else if (args[2] === "server") {
+        // Setting global values
+        port = parseInt(args[3] || port, 10);
+
+        // Start server
+        startServer();
+
     } else {
         help();
     }
@@ -135,6 +175,69 @@ function prepareBuild () {
 
     copyDir(extensionDir, buildDir);
     copy("install.rdf.tpl.xml", path.join(buildDir, "install.rdf"));
+}
+
+// ********************************************************************************************* //
+// Test case
+// ********************************************************************************************* //
+
+function runTest (profile, callback) {
+    var command = "firefox -P " + profile + " -runFBTests --no-remote " + testUrl;
+    var testInstance = exec(command, showError);
+    if (testInstance) {
+        testInstance.on("exit", function(){
+            callback && callback();
+        });
+    } else {
+        callback && callback();
+    }
+}
+
+// ********************************************************************************************* //
+// Simple HTTP Server
+// ********************************************************************************************* //
+
+function startServer () {
+    http.createServer(function(request, response) {
+        var uri = url.parse(request.url).pathname,
+            filename = path.join(process.cwd(), uri);
+
+        var contentTypesByExtension = {
+            '.html': "text/html",
+            '.css':  "text/css",
+            '.js':   "text/javascript"
+        };
+
+        fs.exists(filename, function(exists) {
+            if(!exists) {
+                response.writeHead(404, {"Content-Type": "text/plain"});
+                response.write("404 Not Found\n");
+                response.end();
+                return;
+            }
+
+            if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+            fs.readFile(filename, "utf8", function(err, file) {
+                if(err) {
+                    response.writeHead(500, {"Content-Type": "text/plain"});
+                    response.write(err + "\n");
+                    response.end();
+                    return;
+                }
+
+                var headers = {},
+                    contentType = contentTypesByExtension[path.extname(filename)];
+
+                if (contentType) headers["Content-Type"] = contentType;
+                response.writeHead(200, headers);
+                response.write(file, "utf8");
+                response.end();
+            });
+        });
+    }).listen(port);
+
+    console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
 }
 
 // ********************************************************************************************* //
